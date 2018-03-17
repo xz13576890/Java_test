c00d0f262e22f86d51d925e923a8f11171d5e04e
u6469050 <u6469050@anu.edu.au>
2018-03-18 00:48:11 +1100

    add Test_OOP_this

---
+++ src/Test_OOP_inheritance/Animal.java
@@ -0,0 +1,57 @@
+package Test_OOP_inheritance;
+
+public class Animal /*extends Object*/{ //定义一个类时如果没有指定继承，则默认其父类是Object（Java中的根类）
+    String eyes;
+
+    public void run(){
+        System.out.println("RUN!!");
+    }
+    public void eat(){
+        System.out.println("EAT!!");
+    }
+    public void sleep(){
+        System.out.println("zZZZZ!!");
+    }
+
+}
+
+class Mammal extends Animal {  //Mammal扩展自Animal,所以以下部分跟Animal一样的内容就不用再写了
+    /*
+    String eyes;
+
+    public void run(){
+        System.out.println("RUN!!");
+    }
+    public void eat(){
+        System.out.println("EAT!!");
+    }
+    */
+
+    public void taisheng(){
+        System.out.println("I am viviparous!!");
+
+    }
+
+}
+
+class Reptiles extends Animal { //Reptiles扩展自Animal,所以以下部分跟Animal一样的内容就不用再写了
+
+    /*
+    String eyes;
+    */
+
+    @Override
+    public void run(){
+        System.out.println("Crawl!!"); //但这部分跟Animal有区别，所以可以重写（Override）
+    }
+    /*
+    public void eat(){
+        System.out.println("EAT!!");
+    }
+    */
+
+    public void eggSheng(){
+        System.out.println("I am Oviparous!!");
+    }
+
+}
