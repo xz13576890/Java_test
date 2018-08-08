b86142cc7a6fb921152ec94a96ada8a58822b7af
u6469050 <u6469050@anu.edu.au>
2018-08-08 21:12:34 +1000

    update Prime

--- src/Test_BasicJava_Concept/Prime_number.java
+++ src/Test_BasicJava_Concept/Prime_number.java
@@ -1,7 +1,24 @@
 package Test_BasicJava_Concept;
 
 public class Prime_number {
-    //输出1-100之间的所有质数
+
+    //平方根计算法
+    public static boolean isPrime(int a) {
+        if (a < 2) { // 素数不小于2
+            return false;
+        }else if (a == 2){
+            return true;
+        } else {
+            for (int i = 2; i <= Math.sqrt(a); i++) {
+                if (a % i == 0) { // 若能被整除，则说明不是素数，返回false
+                    return false;
+                }
+            }
+            return true;
+        }
+
+    }
+    //输出1-100之间的所有质数（while循环法）
         public static void main(String[] args) {
             int j;
             for (int i = 2; i <= 100; i++) // 1不是素数，所以直接从2开始循环
